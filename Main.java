21ed35a1440daa71a609294bc70e057038c1a1cd
IluaPlank <gerevan1994@gmail.com>
2021-11-06 17:21:28 +0500

    реализовал все методы из заданий

--- Main.java
+++ Main.java
@@ -1,7 +1,4 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.zip.*;
 
 public class Main {
@@ -20,7 +17,26 @@ public class Main {
                 "D://netologi//Games//savegames//save3.dat");
 
         deleteSave("D://netologi//Games//savegames");
+
+
+        zipUnpack("D://netologi//Games//savegames//Saves.zip","D://netologi//Games//savegames");
+
+        saveUnpack("D://netologi//Games//savegames//save1.dat",
+                "D://netologi//Games//savegames//save2.dat",
+                "D://netologi//Games//savegames//save3.dat");
         }
+    public static void savegame(GameProgress game, int numberSave){
+        try (FileOutputStream fos = new FileOutputStream("D://netologi//Games//savegames//save"+numberSave+".dat");
+             ObjectOutputStream oos = new ObjectOutputStream(fos))
+        {
+            oos.writeObject(game);
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+
     public static void zipSave(String ... files) {
         try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("D://netologi//Games//savegames//Saves.zip"))) {
             for (String file : files) {
@@ -38,18 +54,6 @@ public class Main {
         }
     }
 
-    public static void savegame(GameProgress game, int numberSave){
-        try (FileOutputStream fos = new FileOutputStream("D://netologi//Games//savegames//save"+numberSave+".dat");
-             ObjectOutputStream oos = new ObjectOutputStream(fos))
-        {
-            oos.writeObject(game);
-        }
-        catch (Exception ex)
-        {
-            System.out.println(ex.getMessage());
-        }
-    }
-
     public static void deleteSave(String path){
         File dir = new File(path);
         if (dir.isDirectory()){
@@ -63,6 +67,43 @@ public class Main {
                     }
                 }
             }
+        }
+    }
+
+    public static void zipUnpack(String pathZip, String pathSave) {
+        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(pathZip))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName();
+                // получим название файла
+                // распаковка
+                FileOutputStream fout = new FileOutputStream(pathSave+"//"+name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void saveUnpack(String ... files) {
+        GameProgress gameProgress = null;
+        for (int i=1;i <= files.length;i++) {
+            // откроем входной поток для чтения файла
+            try (FileInputStream fis = new FileInputStream("D://netologi//Games//savegames//save" + i + ".dat");
+                 ObjectInputStream ois = new ObjectInputStream(fis))
+            {
+                // десериализуем объект и скастим его в класс
+                gameProgress = (GameProgress) ois.readObject();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+            System.out.println(gameProgress);
         }
     }
 }
